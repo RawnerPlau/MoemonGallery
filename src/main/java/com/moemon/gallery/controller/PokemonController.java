7a9d18e18a70d8f580877a0d46c650e0163846d3
RawnerPlau <delosreyeswp@gmail.com>
2025-11-06 17:09:36 +0800

    feat(PokemonController): created an API for getting pokemon through an identifier.

--- src/main/java/com/moemon/gallery/controller/PokemonController.java
+++ src/main/java/com/moemon/gallery/controller/PokemonController.java
@@ -2,7 +2,9 @@ package com.moemon.gallery.controller;
 
 import com.moemon.gallery.model.Pokemon;
 import com.moemon.gallery.service.PokemonService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,13 @@ public class PokemonController {
     public List<Pokemon> getAllPokemon() {
         return pokemonService.getAllPokemon();
     }
+
+
+    @GetMapping("/{identifier}")
+    public ResponseEntity<Pokemon> getPokemon(@PathVariable String identifier) {
+        Pokemon pokemon = pokemonService.getPokemonById(identifier);
+        return ResponseEntity.ok(pokemon);
+    }
+
 
 }
